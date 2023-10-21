c015dceec2458eb4c44d7736744d5c4eb9c10044
Suliko1979 <65030395+Suliko1979@users.noreply.github.com>
2023-10-21 16:26:52 -0400

    Update UIRegressionRunner.java

--- digital-bank-acceptance-test/src/test/java/co/wedevx/digitalbank/automation/ui/runners/UIRegressionRunner.java
+++ digital-bank-acceptance-test/src/test/java/co/wedevx/digitalbank/automation/ui/runners/UIRegressionRunner.java
@@ -9,6 +9,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("ui.features")
 @ConfigurationParameter(key=GLUE_PROPERTY_NAME, value="co.wedevx.digitalbank.automation.ui.steps")
-public class UIRegressionRunner {
+public class UIRegressionRunner {//no
 
 }
